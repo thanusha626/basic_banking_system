c3177008bd4ddeb215806c8f496a4c3b6be5203d
thanusha626 <113509051+thanusha626@users.noreply.github.com>
2022-09-20 17:34:03 +0530

    Add files via upload

---
+++ connection page.php
@@ -0,0 +1,29 @@
+
+<?php
+
+$username = "root";
+$password="";
+$server ="localhost";
+$db = "basic_bank";
+
+
+$conn = mysqli_connect($server,$username,$password,$db);
+
+/*if($conn)
+//echo "Connection successful";
+{
+    ?>
+    <script>
+        alert('Connection Successful');
+    </script>
+    <?php
+}
+else
+{
+    echo "No connection";
+    die("no connection".mysqli_connect_error());//exit
+}*/
+
+
+
+?>
